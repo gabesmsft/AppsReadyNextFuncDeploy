9e006a0e9a9c11ffaf47aa12ca6061e7be535592
Gabe Shapiro <gabeshap@outlook.com>
2020-07-22 00:19:01 -0700

    1

--- deploy.cmd
+++ deploy.cmd
@@ -55,15 +55,16 @@ echo Handling function App deployment with Msbuild16.
 
 :: 1. Restore, Build and publish
 call :ExecuteCmd "%MSBUILD_16_DIR%\MSBuild.exe" /restore "%DEPLOYMENT_SOURCE%\AppsReadyNextFuncDeploy\AppsReadyNextFuncDeploy.csproj" /p:DeployOnBuild=true /p:configuration=Release /p:publishurl="%DEPLOYMENT_TEMP%" %SCM_BUILD_ARGS%
+IF !ERRORLEVEL! NEQ 0 goto error
 
+:: 2. Custom deployment script actions
 mkdir %DEPLOYMENT_TEMP%\CreatedByCustomDeploymentScript
 npm install node-time --prefix  %DEPLOYMENT_TEMP%\CreatedByCustomDeploymentScript
-
 IF !ERRORLEVEL! NEQ 0 goto error
 
 mkdir %DEPLOYMENT_TARGET%\CreatedByCustomDeploymentScript1
 
-:: 2. KuduSync
+:: 3. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
